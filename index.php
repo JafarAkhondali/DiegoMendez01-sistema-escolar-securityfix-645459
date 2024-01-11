70706da588cd10d4b4d1190fc975e1ac7c63d0d5
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-01-11 13:12:29 -0500

    Agregar estilos para la visualizacion del login e imagen de referencia

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 </head>
 <body>
 	<header class="main-header">
+		<div class="main-cont">
+			<div class="desc-header">
+				<img src="images/school.svg" alt="Image School" />
+				<p>School</p>
+			</div>
+		</div>
 	</header>
 	<script src="js/jquery-3.7.0.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
