15bbf6e77c7676705648f8c48948f326eaff3711
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-01-11 12:40:05 -0500

    Base de la vista de inicio de sesion para el usuario

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UFT-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+	<title>Ingreso al Sistema</title>
+</head>
+<body>
+	<header class="main-header">
+	</header>
+	<script src="js/jquery-3.7.0.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
