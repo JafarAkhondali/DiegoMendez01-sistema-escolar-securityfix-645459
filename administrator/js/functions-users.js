5f9ec084ac70613ad8b1289a6c145898252ac423
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-01-13 19:58:46 -0500

    Agregar modelo de usuarios para listar los usuarios en el datatables

--- administrator/js/functions-users.js
+++ administrator/js/functions-users.js
@@ -1,5 +1,34 @@
 $('#tableusers').DataTable();
 
+var tableusers;
+
+document.addEventListener('DOMContentLoaded', function(){
+	tableusers = $('#tableusers').DataTable({
+		"aProcessing": true,
+		"aServerSide": true,
+		"language": {
+			"url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/spanish.json"
+		},
+		"ajax": {
+			"url": "./models/users/table_users.php",
+			"dataSrc": ""
+		},
+		"columns": [
+			{"data": "acciones"},
+			{"data": "id"},
+			{"data": "name"},
+			{"data": "user"},
+			{"data": "role_id"},
+			{"data": "is_active"},
+			{"data": "created"}
+		],
+		"responsive": true,
+		"bDestroy": true,
+		"iDisplayLength": 10,
+		"order": [[0, "asc"]]
+	});
+});
+
 function openModal()
 {
 	$('#modalUser').modal('show');
---
+++ administrator/models/users/table_users.php
@@ -0,0 +1,31 @@
+<?php 
+
+require_once '../../../includes/connection.php';
+
+$sql = '
+    SELECT
+        *
+    FROM
+        users;
+';
+
+$query = $pdo->prepare($sql);
+$query->execute();
+
+$consulta = $query->fetchAll(PDO::FETCH_ASSOC);
+
+for($i = 0; $i < count($consulta); $i++){
+    if($consulta[$i]['is_active'] == 1){
+        $consulta[$i]['is_active'] = '<span class="me-1 badge bg-success">Activo</span>';
+    }else{
+        $consulta[$i]['is_active'] = '<span class="me-1 badge bg-danger">Inactivo</span>';
+    }
+    
+    $consulta[$i]['acciones'] = '
+        <button class="btn btn-primary" title="Editar" onclick="edituser('.$consulta[$i]['id'].')">Editar</button>
+        <button class="btn btn-danger" title="Eliminar" onclick="deleteuser('.$consulta[$i]['id'].')">Eliminar</button>
+    ';
+}
+
+echo json_encode($consulta, JSON_UNESCAPED_UNICODE);
+?>
