228b7baf1069a608bd84fcdeab87e1770a2cc906
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-01-17 11:05:30 -0500

    Agregar los cursos que maneja el docente y visualizacion de las opciones de Mis Cursos en el nav del rol docente

--- teacher/includes/nav.php
+++ teacher/includes/nav.php
@@ -1,3 +1,32 @@
+<?php 
+
+require_once '../includes/connection.php';
+
+$id = $_SESSION['id'];
+
+
+$sql = '
+    SELECT
+        tc.id,
+        cs.name as nameCourse,
+        c.name as nameClassroom,
+        t.name as nameTeacher,
+        d.name as nameDegree
+    FROM
+        teacher_courses as tc
+    INNER JOIN degrees d ON tc.degree_id = d.id
+    INNER JOIN classrooms c ON tc.classroom_id = c.id
+    INNER JOIN teachers t ON tc.teacher_id = t.id
+    INNER JOIN courses cs ON tc.course_id = cs.id
+    WHERE
+        tc.is_active = 1 AND tc.teacher_id = ?
+';
+
+$query = $pdo->prepare($sql);
+$query->execute([$id]);
+$row = $query->rowCount();
+?>
+
 <!-- Sidebar menu-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
@@ -8,12 +37,24 @@
     </div>
   </div>
   <ul class="app-menu">
-    <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fas fa-laptop"></i><span class="app-menu__label">UI Elements</span><i class="treeview-indicator bi bi-chevron-right"></i></a>
+    <li><a class="app-menu__item" href="index.php"><i class="app-menu__icon fas fa-home"></i><span class="app-menu__label">Inicio</span></a></li>
+    <li class="treeview">
+      <a class="app-menu__item" href="#" data-toggle="treeview">
+        <i class="app-menu__icon fas fa-laptop"></i>
+          <span class="app-menu__label">Mis Cursos</span>
+        <i class="treeview-indicator bi bi-chevron-right"></i>
+      </a>
       <ul class="treeview-menu">
-        <li><a class="treeview-item" href="#"><i class="icon fas fa-circle"></i> Bootstrap Elements</a></li>
-        <li><a class="treeview-item" href="#" target="_blank" rel="noopener"><i class="icon fas fa-circle"></i> Font Icons</a></li>
-        <li><a class="treeview-item" href="#"><i class="icon fas fa-circle"></i> Cards</a></li>
-        <li><a class="treeview-item" href="#"><i class="icon fas fa-circle"></i> Widgets</a></li>
+      <?php 
+      if($row > 0){
+          while($data = $query->fetch()){
+              ?>
+              <li><a class="treeview-item" href="#<?= $data['id'] ?>"><i class="icon fas fa-circle"></i>
+              <?= $data['nameCourse']; ?> - <?= $data['nameDegree']; ?> - <?= $data['nameClassroom'] ?></a></li>
+              <?php
+          }
+      }
+      ?>
       </ul>
     </li>
     <li><a class="app-menu__item" href="../logout.php"><i class="app-menu__icon fas fa-sign-out-alt"></i><span
--- teacher/index.php
+++ teacher/index.php
@@ -1,25 +1,63 @@
 <?php 
 
 require_once 'includes/header.php';
+require_once '../includes/connection.php';
+
+$id = $_SESSION['id'];
+
+$sql = '
+    SELECT
+        tc.id,
+        cs.name as nameCourse,
+        c.name as nameClassroom,
+        t.name as nameTeacher,
+        d.name as nameDegree
+    FROM
+        teacher_courses as tc
+    INNER JOIN degrees d ON tc.degree_id = d.id
+    INNER JOIN classrooms c ON tc.classroom_id = c.id
+    INNER JOIN teachers t ON tc.teacher_id = t.id
+    INNER JOIN courses cs ON tc.course_id = cs.id
+    WHERE
+        tc.is_active = 1 AND tc.teacher_id = ?
+';
+
+$query = $pdo->prepare($sql);
+$query->execute([$id]);
+$row = $query->rowCount();
 
 ?>
 <main class="app-content">
-  <div class="app-title">
-    <div>
-      <h1><i class="bi bi-speedometer"></i> Blank Page</h1>
-      <p>Start a beautiful journey here</p>
+  <div class="row">
+    <div class="col-md-12 border-shadow p-2 bg-info text-white">
+      <h3>Sistema Escolar - COLPAZ</h3>
     </div>
-    <ul class="app-breadcrumb breadcrumb">
-      <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
-      <li class="breadcrumb-item"><a href="#">Blank Page</a></li>
-    </ul>
   </div>
   <div class="row">
-    <div class="col-md-12">
-      <div class="tile">
-        <div class="tile-body">Create a beautiful dashboard</div>
-      </div>
+    <div class="col-md-12 text-center border mt-3 p-4 bg-light">
+      <h3>Mis Cursos</h3>
     </div>
+  </div>
+  <div class="row">
+    <?php 
+    if($row > 0){
+        while($data = $query->fetch()){
+        ?>
+        <div class="col-md-4 text-center border mt-3 p-4 bg-light">
+             <div class="card m-2 shadow" style="width: 23rem;">
+                  <img src="images/card-school.svg" class="card-img-top" alt="..." style="height: 13rem;"/>
+                  <div class="card-body">
+                       <h4 class="card-title text-center"><?php echo $data['nameCourse']; ?></h4>
+                       <h5 class="card-title">Grado <kbd class="bg-info"><?php echo $data['nameDegree']; ?></kbd> - Aula <kbd class="bg-info"><?php echo $data['nameClassroom']; ?></kbd></h5>
+                       <a href="#<?php $data['id']; ?>" class="btn btn-primary">Acceder</a>
+                       <a href="#<?php $data['id']; ?>" class="btn btn-warning">Ver Alumnos</a>
+                  </div>
+             </div>
+        </div>
+        <?php
+        }
+    }
+    ?>
   </div>
 </main>
 <?php 
